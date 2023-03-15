618dded0ced2c2c37adc7bd19d58d4d26ef484c1
Leandro Kiefer <cmsleandrokiefer@gmail.com>
2023-03-14 21:12:59 -0300

    Adicionar arquivos de projeto.

---
+++ Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,23 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext() { }
+        public ChapterContext (DbContextOptions<ChapterContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-VCOS33J\\SQLEXPRESS ; Initial Catalog = Chapter; Integrated Security = True; Trust Server Certificate=true");
+            }
+
+           
+        }
+        public DbSet<Livro> Livros { get; set; }
+        public object Livro { get; internal set; }
+    }
+}
